3fb78f739b171acb226ca1db85c708f56ca1ca3e
vgb <readsucceednws@gmail.com>
2026-03-15 10:58:24 +0530

    Update use-auth.ts

--- Rejection-Mapper/client/src/hooks/use-auth.ts
+++ Rejection-Mapper/client/src/hooks/use-auth.ts
@@ -1,4 +1,4 @@
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+aimport { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { getQueryFn } from "@/lib/queryClient";
 
 export type AuthUser = {
