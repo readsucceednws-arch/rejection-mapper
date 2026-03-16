837e2b79d30406480611c547bb2ca969af23badd
vgb <readsucceednws@gmail.com>
2026-03-16 11:31:56 +0530

    Update alert.tsx

--- Rejection-Mapper/client/src/components/ui/alert.tsx
+++ Rejection-Mapper/client/src/components/ui/alert.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
-import { cva } from "class-variance-authority";
+import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
 
 const alertVariants = cva(
-  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
+  "relative w-full rounded-lg border p-4",
   {
     variants: {
       variant: {
@@ -19,22 +19,17 @@ const alertVariants = cva(
   }
 );
 
-type AlertVariant = "default" | "destructive";
-
-type AlertProps = React.HTMLAttributes<HTMLDivElement> & {
-  variant?: AlertVariant;
-};
-
-const Alert = React.forwardRef<HTMLDivElement, AlertProps>(
-  ({ className, variant, ...props }, ref) => (
-    <div
-      ref={ref}
-      role="alert"
-      className={cn(alertVariants({ variant }), className)}
-      {...props}
-    />
-  )
-);
+const Alert = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
+>(({ className, variant, ...props }, ref) => (
+  <div
+    ref={ref}
+    role="alert"
+    className={cn(alertVariants({ variant }), className)}
+    {...props}
+  />
+));
 Alert.displayName = "Alert";
 
 const AlertTitle = React.forwardRef<
